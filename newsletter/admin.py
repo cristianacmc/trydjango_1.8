3251d0a082637a0c00897b8f0247d3605b41a667
cristianacmc <cristianacmc@gmail.com>
2016-04-08 17:37:48 +0100

    Project with Django Registration Redux

---
+++ newsletter/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .forms import SignUpForm
+from .models import SignUp
+
+
+
+class SignUpAdmin(admin.ModelAdmin):
+    list_display = ["__str__", "timestamp", "update"]
+    form = SignUpForm
+    #class Meta:
+    #    model = SignUp
+
+
+
+
+admin.site.register(SignUp, SignUpAdmin)
---
+++ src/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+
+urlpatterns = [
+    # Examples:
+    url(r'^$', 'newsletter.views.home', name='home'),
+    url(r'^contact/$', 'newsletter.views.Contact', name='contact'),
+    url(r'^about/$', 'src.views.about', name='about'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include('registration.backends.default.urls')),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.MEDIA_ROOT)
